116c069ba0e8380f8e8c8c00af5c032a3fc22a09
Osman Yiğit Uygun <osmanuygun197@gmail.com>
2026-02-07 13:29:09 +0300

    os module for OS info

---
+++ NodeJS_Crash/os_demo.js
@@ -0,0 +1,9 @@
+import os from "os";
+
+console.log("OS Info: ", os.platform());
+console.log("OS Architecture: ", os.arch());
+console.log("OS CPU Info: ", os.cpus());
+console.log("OS Free Memory: ", os.freemem());
+console.log("OS Total Memory: ", os.totalmem());
+console.log("OS Home Directory: ", os.homedir());
+console.log("User Info: ", os.userInfo())
