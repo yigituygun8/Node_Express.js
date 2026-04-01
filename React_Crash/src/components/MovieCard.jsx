4461dd4b6247953193c255e6400939b760cc1fa1
Osman Yiğit Uygun <osmanuygun197@gmail.com>
2026-04-01 22:43:56 +0300

    MovieCard and Spinner (Loading) Components

---
+++ React_Crash/src/components/MovieCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const MovieCard = ({ movie: { title, vote_average, poster_path, release_date, original_language } }) => {
+  return (
+    <li className='movie-card'>
+        <img src={poster_path ? `https://image.tmdb.org/t/p/w500${poster_path}` : '/no-movie.jpg'} alt={title} />
+        <div className='mt-4 text-white'>
+            <h3>{title}</h3>
+            <div className='content'>
+                <div className='rating'>
+                    <img src="star.svg" alt="Star Icon" />
+                    <p>{vote_average ? vote_average.toFixed(1) : 'N/A'}</p>
+                </div> {/* end of rating */}
+                <span>•</span>
+                <p className='year'>{release_date ? release_date.split('-')[0] : 'N/A'}</p>
+                <span>•</span>
+                <p className='lang'>{original_language.toUpperCase()}</p>
+            </div> {/* end of content */}
+        </div>  
+    </li>
+  )
+}
+
+export default MovieCard
