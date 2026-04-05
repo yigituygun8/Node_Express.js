ce20782b39ee70a7892a3cd86153c3c5db35983c
Osman Yiğit Uygun <osmanuygun197@gmail.com>
2026-04-05 15:16:57 +0300

    Updated no-movie.jpg to no-movie.png

--- React_Crash/src/components/MovieCard.jsx
+++ React_Crash/src/components/MovieCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const MovieCard = ({ movie: { title, vote_average, poster_path, release_date, original_language } }) => {
   return (
     <li className='movie-card'>
-        <img src={poster_path ? `https://image.tmdb.org/t/p/w500${poster_path}` : '/no-movie.jpg'} alt={title} />
+        <img src={poster_path ? `https://image.tmdb.org/t/p/w500${poster_path}` : '/no-movie.png'} alt={title} />
         <div className='mt-4 text-white'>
             <h3>{title}</h3>
             <div className='content'>
