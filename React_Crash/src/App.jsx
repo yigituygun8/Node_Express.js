c904a0d5d44df8959da94b220b5dcfd1807b1cd3
Osman Yiğit Uygun <osmanuygun197@gmail.com>
2026-04-05 15:17:31 +0300

    Debouncing + trending movies + UI changes

--- React_Crash/src/App.jsx
+++ React_Crash/src/App.jsx
@@ -3,6 +3,7 @@ import Search from './components/Search'
 import MovieCard from './components/MovieCard';
 import './App.css';
 import Spinner from './components/Spinner';
+import { updateSearchCount, getTrendingMovies } from './appwrite';
 
 const API_BASE_URL = "https://api.themoviedb.org/3"; // Base URL for TMDB API
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -18,17 +19,29 @@ const App = () => {
   const [searchTerm, setSearchTerm] = useState(''); // State to hold the search term
   // We did not put it in Search component because we want to use it in App component as well, for example to display the search term in the header or to pass it to other
   // We will pass the searchTerm and setSearchTerm as props to the Search component so that it can update the search term when the user types in the search input
+  const [debouncedSearch, setDebouncedSearch] = useState(""); // State to hold the debounced search term, which will be updated after a delay when the user stops typing. This helps to reduce the number of API calls and improve performance.
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedSearch(searchTerm); // update the debounced search term after a delay of 500ms
+    }, 500);
+    return () => clearTimeout(timer); // clear the timer if the component unmounts or if the searchTerm changes before the timer finishes, to prevent memory leaks and unnecessary updates (cleanup function)
+  }, [searchTerm]); // this useEffect hook will run whenever the searchTerm state changes, and it will set a timer to update the debouncedSearch state after 500ms. If the searchTerm changes again before the timer finishes, the previous timer will be cleared and a new one will be set.
   
+  // Instead of this manual useEffect debouncing, we can npm install react-use and use the useDebounce hook from that library, which provides a cleaner and more efficient way to handle debouncing in React. It abstracts away the timer logic and provides a simple interface for debouncing any value.
+  // useDebounce(() => setDebouncedSearch(searchTerm), 500, [searchTerm]);
+
   const [errorMessage, setErrorMessage] = useState('');
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [trendingMovies, setTrendingMovies] = useState([]);
 
-  const fetchMovies = async() => {
+  const fetchMovies = async (query = "") => {
     try {
       setIsLoading(true); // set loading state to true before starting the fetch
       setErrorMessage(''); // clear any previous error messages before starting the fetch
       
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const endpoint = query ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}` : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
       const response = await fetch(endpoint, API_OPTIONS);
       if(!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`); // fetch does not throw an error for HTTP errors (like 404 or 500), so we need to check the response status and throw an error ourselves if it's not ok. It throws an error for network errors (like no internet connection), but not for HTTP errors.
@@ -37,18 +50,36 @@ const App = () => {
       const data = await response.json(); // convert the JSON to a JavaScript object
 
       setMovies(data.results || []); // set the movies state with the results from the API, or an empty array if there are no results
+
+      if(query && data.results.length > 0)  {
+        await updateSearchCount(query, data.results[0]); // call the updateSearchCount function to update the search count in Appwrite whenever we fetch movies. This allows us to track how many times users have searched for movies in our app.
+      }
+
     } catch (error) {
       console.error("Error fetching movies: ", error);
       setErrorMessage("Failed to fetch movies. Please try again later.");
     } finally {
       setIsLoading(false); // set loading state to false in the finally block to ensure it happens regardless of success or error
     }
-  }
+  } // fetchMovies
+
+  const loadTrendingMovies = async () => {
+    try {
+      const result = await getTrendingMovies(); // call the getTrendingMovies function to fetch the trending movies from Appwrite. This function should return the top 5 trending movies based on the search count in our database.
+      setTrendingMovies(result.rows); // update the trendingMovies state with the fetched results
+    } catch (error) {
+      console.error("Error loading trending movies: ", error);
+    }
+  }; // loadTrendingMovies
   
   useEffect(() => {
-    fetchMovies();
-  }, []); // empty dependency array - run only once when the component mounts
+    fetchMovies(debouncedSearch);
+  }, [debouncedSearch]); // whenever the debouncedSearch state changes, the useEffect hook will run the fetchMovies function to fetch movies based on the new search term. This allows us to update the movie list whenever the user types in a new search term.
 
+  useEffect(() => {
+    loadTrendingMovies(); // load the trending movies when the component mounts
+  }, []);
+  
   return (
     <main>
       <div className='pattern'>
@@ -58,8 +89,23 @@ const App = () => {
             <h1>Find <span className='text-gradient'>Great</span> Movies You'll Enjoy Without the Hassle</h1>
             <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
           </header>
+
+          {trendingMovies.length > 0 && (
+            <section className="trending">
+              <h2>Trending Movies</h2>
+              <ul>
+                {trendingMovies.map((movie, index) => (
+                  <li key={movie.$id}>
+                    <p>{index + 1}</p>
+                    <img src={movie.poster_url} alt={movie.title} />
+                  </li>
+                ))}
+              </ul>
+            </section>
+          )}
+
           <section className="all-movies">
-            <h2 className='mt-10'>All Movies</h2>
+            <h2>All Movies</h2>
             {isLoading ? (
               <Spinner />
             ) : errorMessage ? (
