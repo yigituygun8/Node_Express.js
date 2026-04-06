440ec7e3e5dfec7fefbe2ac7c74a90cd2ef75305
Osman Yiğit Uygun <osmanuygun197@gmail.com>
2026-04-06 23:21:14 +0300

    Added Link tag for clicking on movie card

--- React_Crash/src/components/MovieCard.jsx
+++ React_Crash/src/components/MovieCard.jsx
@@ -1,21 +1,25 @@
-const MovieCard = ({ movie: { title, vote_average, poster_path, release_date, original_language } }) => {
+import { Link, NavLink } from "react-router";
+
+const MovieCard = ({ movie: { id, title, vote_average, poster_path, release_date, original_language } }) => {
   return (
-    <li className='movie-card'>
-        <img src={poster_path ? `https://image.tmdb.org/t/p/w500${poster_path}` : '/no-movie.png'} alt={title} />
-        <div className='mt-4 text-white'>
-            <h3>{title}</h3>
-            <div className='content'>
-                <div className='rating'>
-                    <img src="star.svg" alt="Star Icon" />
-                    <p>{vote_average ? vote_average.toFixed(1) : 'N/A'}</p>
-                </div> {/* end of rating */}
-                <span>•</span>
-                <p className='year'>{release_date ? release_date.split('-')[0] : 'N/A'}</p>
-                <span>•</span>
-                <p className='lang'>{original_language.toUpperCase()}</p>
-            </div> {/* end of content */}
-        </div>  
-    </li>
+    <Link to={`/movie/${id}`} className="movie-card-link">
+        <li className='movie-card'>
+            <img src={poster_path ? `https://image.tmdb.org/t/p/w500${poster_path}` : '/no-movie.png'} alt={title} />
+            <div className='mt-4 text-white'>
+                <h3>{title}</h3>
+                <div className='content'>
+                    <div className='rating'>
+                        <img src="star.svg" alt="Star Icon" />
+                        <p>{vote_average ? vote_average.toFixed(1) : 'N/A'}</p>
+                    </div> {/* end of rating */}
+                    <span>•</span>
+                    <p className='year'>{release_date ? release_date.split('-')[0] : 'N/A'}</p>
+                    <span>•</span>
+                    <p className='lang'>{original_language.toUpperCase()}</p>
+                </div> {/* end of content */}
+            </div>  
+        </li>
+    </Link>
   )
 }
 
