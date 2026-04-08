72374242ff0c557f9c744018acc9d22d094e93be
Osman Yiğit Uygun <osmanuygun197@gmail.com>
2026-04-08 17:11:31 +0300

    ProfilePage component created

---
+++ React_Crash/src/components/ProfilePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+/*
+Simple profile page, content:
+- User's name
+- User's profile picture
+- User's favorite movies (can be a list of movie cards, similar to the ones on the homepage)
+*/
+
+const ProfilePage = () => {
+  return (
+    <div>Profile Page</div>
+  )
+}
+
+export default ProfilePage
